fc57cdfbd837ee997b191070348160c362de2374
Luca10992 <lucalillo.99.1@gmail.com>
2024-01-10 23:34:56 +0100

    feat. add milestone 2 and bonus

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const distanceImput = document.getElementById("distance");
 const ageImput = document.getElementById("age");
 const buttonImput = document.getElementById("generate");
 const priceTicket = document.getElementById("price-ticket");
+const usernameTicket = document.getElementById("username-ticket");
+const offertaTicket = document.getElementById("offerta-ticket");
 
 const priceStandard = 0.21;
 
@@ -13,21 +15,28 @@ buttonImput.addEventListener('click', function() {
     console.log('Km da percorrere: ' + distance);
     const age = ageImput.value;
     console.log('Fascia di età: ' + age);
-
+    
+    const baby = "baby"
+    const old = "old"
+    const standard = "standard"
     const priceKm = priceStandard * distance;
     
     if (age == "minorenne") {
         discount = 20;
+        offertaTicket.innerHTML = 'Biglietto: ' + baby; 
     } else if (age == "ov65") {
         discount = 40;
+        offertaTicket.innerHTML = 'Biglietto: ' + old;
     } else {
         discount = 0;
+        offertaTicket.innerHTML = 'Biglietto: ' + standard;
     }
     
     const finalPrice = priceKm - (priceKm * discount / 100);
     const priceText = "€" + finalPrice.toFixed(2);
     console.log('Prezzo biglietto: ' + priceText);
 
+    usernameTicket.innerText = username;
     priceTicket.innerHTML = priceText;
 })
 
